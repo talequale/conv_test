67872b05f85eda54ab939c26f8f30991e06e3aeb
talequale <antonova.julia@gmail.com>
2024-04-27 23:41:50 +0400

    initial commit

---
+++ conv_test/src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import Obj from './components/Obj.vue'
+</script>
+
+<template>
+  <header>
+    
+
+    
+  </header>
+
+  <main>
+    <!-- <Obj></Obj> -->
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+}
+</style>
---
+++ conv_test/src/components/Obj.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
+
+const loader = new OBJLoader();
+
+// load a resource
+loader.load(
+	// resource URL
+	'assets/obj/minicity.obj',
+	// called when resource is loaded
+	function ( object ) {
+
+		scene.add( object );
+
+	},
+	// called when loading is in progresses
+	function ( xhr ) {
+
+		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+	},
+	// called when loading has errors
+	function ( error ) {
+
+		console.log( 'An error happened' );
+
+	}
+);
+</script>
+
+<template>
+  <div class="obj">
+    
+  </div>
+</template>
+
+<style scoped>
+
+</style>
